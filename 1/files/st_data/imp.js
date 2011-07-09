b09b844fbd863b54a1720b67dc1a5afa935e2aac
hbarnor <henry DOT barnor AT sv.cmu.edu>
2011-07-09 14:30:06 -0700

    Combined css

---
+++ 1/files/st_data/imp.js
@@ -0,0 +1,6 @@
+document.write('<iframe allowtransparency=\"true\" scrolling=\"no\" marginwidth=\"0\" marginheight=\"0\" frameborder=\"0\" height=\"90\" width=\"728\" src=\"http://ad.xtendmedia.com/iframe3?pFEEAM6bBQCCKY4AAAAAAC.bIwAAAAAAAgAEAAYAAAAAAP8AAAAHD4-AJQAAAAAABNsEAAAAAABNCy8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAmEAMAAAAAAAIAAwAAAAAAAAAAAACA4z8AAAAAAIDjPwAAAAAAAO4.AAAAAAAA7j8AAAAAAAD0PwAAAAAAAPQ.AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACjILVQ3HFYCvJ7JqVoHxukGBJWu8RfDrf5XHCMAAAAAA==,,http%3A%2F%2Fninjakiwi.com%2F,B%3D10%26Z%3D728x90%26_salt%3D1381415336%26r%3D0%26s%3D367566,26fa3af8-a4f2-11e0-aab1-bf7f5e8a647e\"></iframe>');
+var rm_data = new Object();
+rm_data.creative_id = 9316738;
+rm_data.offer_type = 14;
+rm_data.entity_id = 289820;
+if (window.rm_crex_data) {rm_crex_data.push(9316738);}
